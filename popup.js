47cf248609606b9f19061e10719e9a0dc9486b33
AlvinManojAlex <alvinmanoj02@gmail.com>
2023-12-30 13:13:13 +0530

    File: Add file required for popup.html

---
+++ popup.js
@@ -0,0 +1,14 @@
+// popup.js
+// This is the file that will handle the button clicks in the popup html file
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('yesButton').addEventListener('click', function() {
+      chrome.runtime.sendMessage({ action: 'workCompleted' });
+      window.close();
+    });
+  
+    document.getElementById('noButton').addEventListener('click', function() {
+      window.close();
+    });
+  });
+  
